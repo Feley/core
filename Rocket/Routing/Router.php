5552d5da8708753888a10bc1bec12e96a4323736
Emmanuel [netesy] Olisah <netesy1@gmail.com>
2020-03-27 17:18:16 +0100

    Autoload all the routes

--- Rocket/Routing/Router.php
+++ Rocket/Routing/Router.php
@@ -8,15 +8,14 @@ use Pecee\SimpleRouter\SimpleRouter;
 class Router extends SimpleRouter
 {
     public static function startNow() {
-
-        // change this to whatever makes sense in your project
-        $dotenv = \Dotenv\Dotenv::createMutable(getenv('BASE_PATH'));
-        $dotenv->load();
-        //$views = getenv('BASE_PATH').getenv('BLADE_VIEW');
         $path = getenv('BASE_PATH');
+        // change this to whatever makes sense in your project
         // change default namespace for all routes
         parent::setDefaultNamespace('\Voom\Controller');
-        require_once($path.'/routes/web.php');
+        foreach (glob($path."/routes/*.php") as $route) {
+            require_once($route);
+        }
+        
         // Do initial stuff
         parent::start();
 
