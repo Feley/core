31b8cb75a105c12863db94bb810df10f90bd3862
Emmanuel [netesy] Olisah <netesy1@gmail.com>
2020-03-29 04:22:43 +0100

    Night work on CLI

--- Rocket/Console/Init.php
+++ Rocket/Console/Init.php
@@ -16,8 +16,19 @@ class Init
     {
         $this->console = new CLImate;
         $this->console->backgroundWhite();
-        $this->console->black();
+        $this->console->red();
         $this->console->bold()->blink()->out("Speed: The Framework For The Fast");
+        $input = $this->console->input("Test Me");
+
+    $response  = $input->prompt();
+    if($response){
+        if ($response == 'controllers') {
+            $this->controllers();
+        }else{
+            $this->keywords();
+        }
+    }
+
     }
 
     public function shout($value='')
@@ -43,6 +54,75 @@ class Init
     public function table(array $value)
     {
         $this->console->table($value);
+    }
+
+    public function quit()
+    {
+        $this->console->close();
+    }
+
+    public function clear()
+    {
+        $this->console->clear();
+    }
+
+    public function controllers()
+    {
+        $this->console->out("Output The controllers");
+    }
+
+    public function keywords()
+    {
+        $this->console->arguments->add([
+    'user' => [
+        'prefix'       => 'u',
+        'longPrefix'   => 'user',
+        'description'  => 'Username',
+        'defaultValue' => 'me_myself_i',
+    ],
+    'password' => [
+        'prefix'      => 'p',
+        'longPrefix'  => 'password',
+        'description' => 'Password',
+        'required'    => true,
+    ],
+    'iterations' => [
+        'prefix'      => 'i',
+        'longPrefix'  => 'iterations',
+        'description' => 'Number of iterations',
+        'castTo'      => 'int',
+    ],
+    'verbose' => [
+        'prefix'      => 'v',
+        'longPrefix'  => 'verbose',
+        'description' => 'Verbose output',
+        'noValue'     => true,
+    ],
+    'help' => [
+        'longPrefix'  => 'help',
+        'description' => 'Prints a usage statement',
+        'noValue'     => true,
+    ],
+    'make' => [
+        'description' => 'Make Command',
+        'make:controller' => [
+            'description' => 'Create a new controller class',
+        ],
+        'make:migration' => [
+            'description' => 'Create a new migration file',
+        ],
+        'make:model' => [
+            'description' => 'Create a new Model class',
+        ],
+        'make:middleware' => [
+            'description' => 'Create a new middleware class',
+        ],
+
+
+
+    ],
+]);
+        $this->console->usage();
     }
 }
 ?>
