b73fa4c1276789b4059a88c3f70712132922f00e
Emmanuel [netesy] Olisah <netesy1@gmail.com>
2020-03-31 08:51:00 +0100

    Lang Work

--- Rocket/View/Lang.php
+++ Rocket/View/Lang.php
@@ -2,13 +2,15 @@
 
 namespace Rocket\View;
 
-use use eftec\bladeone\BladeOneLang;
+use eftec\bladeone\BladeOne; 
+use eftec\bladeone\BladeOneLang;
 
 /**
  * The Language Class for Translation In the Template
  */
-class Lang extends BladeOneLang
+class Lang extends BladeOne
 {
+    use BladeOneLang;
     /**
      * To Use This Class Just
      * use Rocket\View\Lang;
@@ -18,7 +20,18 @@ class Lang extends BladeOneLang
      *      'Cats'=>'Gatos', // plural
      *      '%s is a nice cat'=>'%s es un buen gato'
      * );
-     * /
+     */
 }
-
+    $dotenv = \Dotenv\Dotenv::createMutable(getenv('BASE_PATH'));
+    $dotenv->load();
+    $views = getenv('BASE_PATH').getenv('BLADE_VIEW');
+    $cache = getenv('BASE_PATH').getenv('BLADE_CACHE');
+    $langPath = getenv('BASE_PATH').getenv('BLADE_LANG');
+    $log = getenv('BASE_PATH').'/log.txt';
+    $blade = new BladeOne($views, $cache, BladeOne::MODE_FAST);
+    $blade->setBaseUrl(getenv('APP_URL'));
+    $blade->setIsCompiled(false); 
+    $blade->missingLog=$log; // if a traduction is missing the it will be saved here.
+    $lang='jp'; // try es,jp or fr
+    include $langPath.$lang.'.php';
 ?>
