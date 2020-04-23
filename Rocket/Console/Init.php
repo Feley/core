582d08f6a364d7f1e818226c8bb060088a7e0259
Emmanuel [netesy] Olisah <netesy1@gmail.com>
2020-04-23 23:54:25 +0100

    small changes

--- Rocket/Console/Init.php
+++ Rocket/Console/Init.php
@@ -15,10 +15,13 @@ class Init
     public $console;
     function __construct()
     {
-        $directory = __DIR__.'/Commands/';
-        $app = new Application('Speed Console', '1.0');
-        // foreach (glob($directory."/*.php") as $file) {
-        //     require_once($file);
+        $directory = '/Commands/';
+        $app = new Application('Voom Framework', '1.0');
+        // foreach (glob($directory."*.php") as $file) {
+        //    // echo $file;
+        //     print_r($file);
+        //     var_dump($file);
+        //   // $app->add(new Commands\$file());
         // }
         $app->add(new Commands\InitCommand());
         $app->run();
